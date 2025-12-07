9634602a37ac63e2aea1ee14fe1288cd9a711e51
israel opoku <149181214+IsraelTech-Pro@users.noreply.github.com>
2025-12-07 12:33:59 +0000

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
-# Exit on error
+
 set -o errexit
 
-# Download the .NET SDK
+# Download .NET SDK
 wget https://download.visualstudio.microsoft.com/download/pr/60218cc4-13eb-41d5-aa0b-5fd5a3fb03b8/6c42bee7c3651b1317b709a27a741362/dotnet-sdk-8.0.303-linux-x64.tar.gz
 
-# Create a directory for .NET SDK and extract the downloaded file
+# Extract SDK
 mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-8.0.303-linux-x64.tar.gz -C $HOME/dotnet
 
-# Set environment variables for .NET SDK
+# Set environment variables
 export DOTNET_ROOT=$HOME/dotnet
 export PATH=$PATH:$HOME/dotnet
 
-# Verify the .NET SDK installation
+# Install Blazor WASM tools
+dotnet workload install wasm-tools
+
+# Verify installation
 dotnet --version
 
-# Navigate to the TaskManagerApp directory
+# Navigate to project folder (where .csproj exists)
 cd israelweb
 
-# Publish the .NET application
+# Restore dependencies
+dotnet restore
+
+# Publish project
 dotnet publish -c Release -o out
